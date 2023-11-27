a8315ef86e118a1e8ea3a052465c982331569c01
Ju391bihhtwgkn <Bihl.Julian@web.de>
2023-11-27 23:13:12 +0100

    Changed fetch-calls to Ajax

--- muehle/public/javascripts/board.js
+++ muehle/public/javascripts/board.js
@@ -8,9 +8,20 @@ var number2new = null;
 var cellIDold = null;
 var cellIDnew = null;
 
+
+$(document).ready(function() {
+    // Reagiere auf Klicks auf alle Elemente mit der Klasse 'cell'
+    $('.cell').click(function() {
+        var cellId = $(this).attr('id'); // Hole die ID des geklickten Elements
+        handleButtonClick(cellId);
+    });
+});
+
+
 function handleButtonClick(cellId) {
 
-    var cell = document.getElementById(cellId);
+    var cell = $('#' + cellId);
+    console.log(cell);
     var parts = cellId.split('-'); // Split the cellId by hyphens
 
     if (parts.length < 3) {
@@ -23,61 +34,60 @@ function handleButtonClick(cellId) {
     if(gamestatus == 'put') {
 
         var pnumber1 = parseInt(parts[parts.length - 2]);
+        console.log('pn1: ' + pnumber1);
         var pnumber2 = parseInt(parts[parts.length - 1]);
+        console.log('pn1: ' + pnumber2);
 
-        fetch('/put', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ param1: pnumber1, param2: pnumber2 })
-        })
-        .then(response => response.json())
-        .then(data => {
 
-            
-            console.log('Playerstatus: ' + playerstatus);
+        $.ajax({
+            type: "POST",
+            url: "/put",
+            contentType: "application/json",
+            data: JSON.stringify({ param1: pnumber1, param2: pnumber2}),
+            success: function(data) {
+                console.log('Playerstatus: ' + playerstatus);
             gamestatus = data.value2;
 
             if (playerstatus == 'W') {
-                cell.style.backgroundColor = '#FFFFFF'
+                cell.css('background-color', '#FFFFFF'); // .style.backgroundColor = '#FFFFFF'
             } else if(playerstatus == 'B') {
-                cell.style.backgroundColor = '#FF0000'
+                cell.css('background-color', '#FF0000');
             }
 
             playerstatus = data.value1;
-
+            },
+            error: function(xhr, status, error) {
+                console.log('Error: ', error);
+            }
+    
         })
-        .catch((error) => {
-            console.log('Error:', error);
-            return;
-        });
 
     } else if (gamestatus == 'take') {
 
         var pnumber1 = parseInt(parts[parts.length - 2]);
         var pnumber2 = parseInt(parts[parts.length - 1]);
             
-            fetch('/take', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ param1: pnumber1, param2: pnumber2 })
-            })
-            .then(response => response.json())
-            .then(data => {
+
+        $.ajax({
+            type: "POST",
+            url: "/take",
+            contentType: "application/json",
+            data: JSON.stringify({ param1: pnumber1, param2: pnumber2}),
+            success: function(data) {
 
                 playerstatus = data.value1;
                 console.log('Playerstatus: ' + playerstatus);
                 gamestatus = data.value2;
 
-                cell.style.backgroundColor = '#000000'
-            })
-            .catch((error) => {
-                console.log('Error:', error);
+                cell.css('background-color', '#000000');
 
-            });
+                playerstatus = data.value1;
+            },
+            error: function(xhr, status, error) {
+                console.log('Error: ', error);
+            }
+    
+        })
     
     } else if (gamestatus == 'move') {
         clickCounter++;
@@ -92,33 +102,35 @@ function handleButtonClick(cellId) {
             number1new = parseInt(parts[parts.length - 2]);
             number2new = parseInt(parts[parts.length - 1]);
             cellIDnew = cellId;
-          fetch('/move', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ param1: number1, param2: number2, param3: number1new, param4: number2new})
-        })
-        .then(response => response.json())
-        .then(data => {
 
-            console.log('Playerstatus: ' + playerstatus);
-            gamestatus = data.value2;
+            
+            $.ajax({
+                type: "POST",
+                url: "/move",
+                contentType: "application/json",
+                data: JSON.stringify({ param1: number1, param2: number2, param3: number1new, param4: number2new}),
+                success: function(data) {
+    
+                    console.log('Playerstatus: ' + playerstatus);
+                    gamestatus = data.value2;
 
-            if (playerstatus == 'W') {
-                document.getElementById(cellIDold).style.backgroundColor = '#000000'
-                document.getElementById(cellIDnew).style.backgroundColor = '#FFFFFF'
-            } else if(playerstatus == 'B') {
-                document.getElementById(cellIDold).style.backgroundColor = '#000000'
-                document.getElementById(cellIDnew).style.backgroundColor = '#FF0000'
-            }
-            playerstatus = data.value1;
-
-        })
-        .catch((error) => {
-            console.log('Error:', error);
-
-        });
+                    if (playerstatus == 'W') {
+                        cellIDold.css('background-color' , '#000000');
+                        cellIDnew.css('background-color' , '#FFFFFF');
+                        //document.getElementById(cellIDold).style.backgroundColor = '#000000'
+                    } else if(playerstatus == 'B') {
+                        cellIDold.css('background-color' , '#000000');
+                        cellIDnew.css('background-color' , '#FF0000');
+                        }
+                    
+                        playerstatus = data.value1;
+                },
+                
+                error: function(xhr, status, error) {
+                    console.log('Error: ', error);
+                }
+        
+            })
 
           clickCounter = 0;
           number1 = null;
